692ebf7c82acfcc5631a70d475ec0b4e93844137
Aman Singh <reachout.amansingh@gmail.com>
2021-08-31 02:17:13 +0530

    fix : database.js

--- src/database.js
+++ src/database.js
@@ -1,43 +1,56 @@
 const foodDB = {
   Indian: [
-    { dish: "Chole Bhature" },
-    { dish: "Paneer Butter Masala" },
-    { dish: "Rajma Chawal" },
-    { dish: "Paneer Tikka" },
-    { dish: "Dal Makhani" }
+    { img: "chole-bhature.jpeg", dish: "Chole Bhature", ratings: "4.5/5" },
+
+    {
+      img: "paneer-butter-masala.jpeg",
+      dish: "Paneer Butter Masala",
+      ratings: "4.6/5"
+    },
+
+    { img: "rajma-chawal.jpeg", dish: "Rajma Chawal", ratings: "4.3/5" }
+
+    // { dish: "Paneer Tikka" },
+    // { dish: "Dal Makhani" }
   ],
 
   Italian: [
-    { dish: "Lasagna" },
-    { dish: "Panzanella" },
-    { dish: "Spaghetti" },
-    { dish: "Ossobuco" },
-    { dish: "Arancini" }
+    { img: "lasagna.jpeg", dish: "Lasagna", ratings: "4.2/5" },
+
+    { img: "panzanella.jpeg", dish: "Panzanella", ratings: "4/5" },
+
+    { img: "spagetti.jpeg", dish: "Spaghetti", ratings: "4.3/5" }
+
+    // { dish: "Ossobuco" },
+    // { dish: "Arancini" }
   ],
 
-  French: [
-    { dish: "Soupe à l’oignon" },
-    { dish: "Chocolate soufflé" },
-    { dish: "Flamiche" },
-    { dish: "Tarte Tatin" },
-    { dish: "Salade Niçoise" }
-  ],
+  // French: [
+  //   { dish: "Soupe à l’oignon" },
+  //   { dish: "Chocolate soufflé" },
+  //   { dish: "Flamiche" },
+  //   { dish: "Tarte Tatin" },
+  //   { dish: "Salade Niçoise" }
+  // ],
 
   Japanese: [
-    { dish: "Sushi" },
-    { dish: "Sashimi" },
-    { dish: "Sukiyaki" },
-    { dish: "Onigiri" },
-    { dish: "Tempura" }
-  ],
+    { img: "sushi.jpeg", dish: "Sushi", ratings: "4.5/5" },
 
-  Korean: [
-    { dish: "Kimchi" },
-    { dish: "Bibimbap" },
-    { dish: "Jjajangmyeon" },
-    { dish: "Tteokbokki" },
-    { dish: "Bulgogi" }
+    { img: "sashimi.jpeg", dish: "Sashimi", ratings: "4/5" },
+
+    { img: "sukiyaki.jpeg", dish: "Sukiyaki", ratings: "4.1/5" }
+
+    // { dish: "Onigiri" },
+    // { dish: "Tempura" }
   ]
+
+  // Korean: [
+  //   { dish: "Kimchi" },
+  //   { dish: "Bibimbap" },
+  //   { dish: "Jjajangmyeon" },
+  //   { dish: "Tteokbokki" },
+  //   { dish: "Bulgogi" }
+  // ]
 };
 
 export default foodDB;
