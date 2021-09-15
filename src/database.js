93912ae5aa627402e6d345c8de5137d610c2db73
Aman Singh <reachout.amansingh@gmail.com>
2021-09-16 00:17:10 +0530

    feat : update database

--- src/database.js
+++ src/database.js
@@ -1,56 +1,81 @@
 const foodDB = {
   Indian: [
-    { img: "chole-bhature.jpeg", dish: "Chole Bhature", ratings: "4.5/5" },
+    {
+      img: "chole-bhature.jpeg",
+      dish: "Chole Bhature",
+      ratings: "4.5/5",
+      description:
+        "Chole bhature is a food dish popular in the Northern areas of the Indian subcontinent. It is a combination of chana masala and bhatura/puri, a fried bread made from maida."
+    },
 
     {
       img: "paneer-butter-masala.jpeg",
       dish: "Paneer Butter Masala",
-      ratings: "4.6/5"
+      ratings: "4.6/5",
+      description:
+        "Paneer butter masala, also known as butter paneer is a rich & creamy curry made with paneer, spices, onions, tomatoes, cashews and butter."
     },
 
-    { img: "rajma-chawal.jpeg", dish: "Rajma Chawal", ratings: "4.3/5" }
-
-    // { dish: "Paneer Tikka" },
-    // { dish: "Dal Makhani" }
+    {
+      img: "rajma-chawal.jpeg",
+      dish: "Rajma Chawal",
+      ratings: "4.3/5",
+      description:
+        "Rajma, also known as rajmah, razma, or lal lobia, is a vegetarian dish, originating from the Indian subcontinent, consisting of red kidney beans in a thick gravy with many Indian whole spices, and is usually served with rice."
+    }
   ],
 
   Italian: [
-    { img: "lasagna.jpeg", dish: "Lasagna", ratings: "4.2/5" },
+    {
+      img: "lasagna.jpeg",
+      dish: "Lasagna",
+      ratings: "4.2/5",
+      description:
+        "Lasagne are a type of wide, flat pasta, possibly one of the oldest types of pasta."
+    },
 
-    { img: "panzanella.jpeg", dish: "Panzanella", ratings: "4/5" },
+    {
+      img: "panzanella.jpeg",
+      dish: "Panzanella",
+      ratings: "4/5",
+      description:
+        "Panzanella or panmolle is a Tuscan chopped salad of soaked stale bread, onions and tomatoes that is popular in the summer."
+    },
 
-    { img: "spagetti.jpeg", dish: "Spaghetti", ratings: "4.3/5" }
-
-    // { dish: "Ossobuco" },
-    // { dish: "Arancini" }
+    {
+      img: "spagetti.jpeg",
+      dish: "Spaghetti",
+      ratings: "4.3/5",
+      description:
+        "Spaghetti is a long, thin, solid, cylindrical pasta. It is a staple food of traditional Italian cuisine."
+    }
   ],
 
-  // French: [
-  //   { dish: "Soupe à l’oignon" },
-  //   { dish: "Chocolate soufflé" },
-  //   { dish: "Flamiche" },
-  //   { dish: "Tarte Tatin" },
-  //   { dish: "Salade Niçoise" }
-  // ],
-
   Japanese: [
-    { img: "sushi.jpeg", dish: "Sushi", ratings: "4.5/5" },
+    {
+      img: "sushi.jpeg",
+      dish: "Sushi",
+      ratings: "4.5/5",
+      description:
+        "Sushi is a traditional Japanese dish of prepared vinegared rice, usually with some sugar and salt, accompanied by a variety of ingredients, such as seafood, often raw, and vegetables."
+    },
 
-    { img: "sashimi.jpeg", dish: "Sashimi", ratings: "4/5" },
+    {
+      img: "sashimi.jpeg",
+      dish: "Sashimi",
+      ratings: "4/5",
+      description:
+        "Sashimi is a Japanese delicacy consisting of fresh raw fish or meat sliced into thin pieces and often eaten with soy sauce."
+    },
 
-    { img: "sukiyaki.jpeg", dish: "Sukiyaki", ratings: "4.1/5" }
-
-    // { dish: "Onigiri" },
-    // { dish: "Tempura" }
+    {
+      img: "sukiyaki.jpeg",
+      dish: "Sukiyaki",
+      ratings: "4.1/5",
+      description:
+        "Sukiyaki is a Japanese dish that is prepared and served in the nabemono style."
+    }
   ]
-
-  // Korean: [
-  //   { dish: "Kimchi" },
-  //   { dish: "Bibimbap" },
-  //   { dish: "Jjajangmyeon" },
-  //   { dish: "Tteokbokki" },
-  //   { dish: "Bulgogi" }
-  // ]
 };
 
 export default foodDB;
